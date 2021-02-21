808cbb7d7bd27e0dfa437a6e45e0d6d1cf06a0f6
Jithendrian Sundaravaradan (jitsunda) <jitsunda@cisco.com>
2021-02-22 01:26:36 +0530

    Solving problems

--- GoLang/Practice/primeFactor.go
+++ GoLang/Practice/primeFactor.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+/*
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+ */
+
+var(
+	primeFactors[] int
+	primeNumber int
+)
+
+func isPrime(a int) bool{
+
+	for i:=2; i < a/2; i +=1 {
+		if a%i == 0 {
+			return false
+		}
+	}
+	return true
+}
 func main() {
-	$END$
+	primeNumber = 600851475143
+	i := 2
+	for primeNumber > 2 {
+		if primeNumber % i == 0 && isPrime(i){
+			primeNumber = primeNumber / i
+			primeFactors = append(primeFactors, i)
+		}
+		i = i + 1
+	}
+	fmt.Println(primeFactors)
+	fmt.Println(primeFactors[len(primeFactors) - 1])
+
 }
